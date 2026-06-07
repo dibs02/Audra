0ddea7a3a279b99c5d45a01743b40aaa0e854d75
Dibyajyoti Ganguly <gangulyd49@gmail.com>
2026-06-07 20:41:58 +0530

    Implement job creation in DB on successful file upload

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,6 +1,12 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
+import { PrismaClient } from "@/lib/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const prisma = new PrismaClient({
+  adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL }),
+});
 
 const f = createUploadthing();
 
@@ -23,6 +29,16 @@ export const ourFileRouter = {
     .onUploadComplete(async ({ metadata, file }) => {
       console.log("Upload complete for userId:", metadata.userId);
       console.log("file url", file.ufsUrl);
+
+      const job = await prisma.job.create({
+        data: {
+          userId: metadata.userId,
+          fileUrl: file.ufsUrl,
+          status: "PENDING",
+        },
+      });
+
+      console.log(job);
 
       // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
       return { message: "Upload complete" };
